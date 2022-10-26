b0ad9602def424de760e7fbbd5e8f770f01254b6
Leon Stegmann VM <leon.stegmann@tum.de>
2022-10-26 18:26:37 +0200

    UPDATE Coding challenge II and patch

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include<espl_lib.h>
+#include <espl_lib.h>
 
 int main(void){
 	printf("Hello ESPL\n");
-	printf("This is a new print\n");
-	unsigned int input = getchar();
-	char output;
-	output =  num_to_words(input);
-	printf("%c\n", output);
+	char stop = '1';
+	do {
+		printf("\nPlease enter Number: ");
+		unsigned int input;
+		scanf("%d", &input);
+		char *output =  num_to_words(input);
+		printf("%c\n", *output);
+		printf("Wish to exit? Press 0. Else press any other key: ");
+		while ((getchar()) != '\n');
+		stop = getchar(); //flushes the input buffer
+	} while (stop != '0');
 return 0;
 }
