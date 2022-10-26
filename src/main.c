04d917f2636141318ad4af7f4278143afc7eca7d
Leon Stegmann VM <leon.stegmann@tum.de>
2022-10-26 17:03:16 +0200

    UPDATE Coding challenge II

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include<espl_lib.h>
 
 int main(void){
 	printf("Hello ESPL\n");
 	printf("This is a new print\n");
+	unsigned int input = getchar();
+	char output;
+	output =  num_to_words(input);
+	printf("%c\n", output);
 return 0;
 }
